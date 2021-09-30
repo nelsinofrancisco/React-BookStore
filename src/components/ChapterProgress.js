c3e966de62a211dd126827f22bf99a1a7853261f
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-09-30 14:18:25 -0300

    Add main branch

--- src/components/ChapterProgress.js
+++
@@ -1,8 +0,0 @@
-const ChapterProgress = () => (
-  <div className="chapterProgressContainer">
-    <p className="fontStyle8 fontColor1">5%</p>
-    <p className="fontStyle5 fontColor1 normal">Completed</p>
-  </div>
-);
-
-export default ChapterProgress;
