abb90cbcb2c19381fc387e5b7875b6e38894c443
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-09-30 21:12:07 -0300

    remove linter exception

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,6 @@
-/* eslint-disable import/extensions */
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
-import booksReducer from './books/Books.js';
+import booksReducer from './books/Books';
 
 const reducer = combineReducers({
   books: booksReducer,
