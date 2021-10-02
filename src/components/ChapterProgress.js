832123bc1c6f41113fd0e43870acb82a637cb6cb
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-10-02 10:59:27 -0300

    Add percentage circle

--- src/components/ChapterProgress.js
+++ src/components/ChapterProgress.js
@@ -1,8 +1,29 @@
-const ChapterProgress = () => (
-  <div className="chapterProgressContainer">
-    <p className="fontStyle8 fontColor1">5%</p>
-    <p className="fontStyle5 fontColor1 normal">Completed</p>
-  </div>
-);
+const ChapterProgress = () => {
+  const percentage = Math.floor(Math.random() * 100) / 100;
+  const percentageString = percentage * 100;
+  const circle2 = {
+    stroke: '#0290ff',
+    strokeDasharray: '251',
+    strokeDashoffset: `calc(251 * (1 - ${percentage}))`,
+  };
+
+  return (
+    <div className="chapterProgressContainer">
+      <div className="percentageCircleContainer">
+        <svg className="percentageCircle">
+          <circle className="circle1" cx="40" cy="40" r="40" />
+          <circle style={circle2} cx="40" cy="40" r="40" />
+        </svg>
+      </div>
+      <div>
+        <p className="fontStyle8 fontColor1">
+          {percentageString}
+          %
+        </p>
+        <p className="fontStyle5 fontColor1 normal">Completed</p>
+      </div>
+    </div>
+  );
+};
 
 export default ChapterProgress;
